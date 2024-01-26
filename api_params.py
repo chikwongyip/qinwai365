0543a2d314491b3f08557b99db8495cf5c058dd1
chikwongyip <527439024@qq.com>
2024-01-27 07:32:24 +0800

    visit api

--- api_params.py
+++ api_params.py
@@ -2,16 +2,9 @@
 
 class Params:
     # 初始化
-    def __init__(self, page_number):
-        self.page_number = page_number
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            self['key'] = kwargs[key]
 
 
 #   拜访记录传入参数
-class CusVisitRecord:
-
-    def __init__(self, **kwargs):
-        self.page = kwargs['id']
-        self.date_start = kwargs['date_start']
-        self.date_end = kwargs['date_end']
-        self.visitor_code = kwargs['visitor_code']
-        self.rows = 1000
