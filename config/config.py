7babba20e7bd9564e7c38ffdc44db3bc3dd4f5e0
chikwong <527439024@qq.com>
2024-09-27 11:52:22 +0800

    qince API handle

--- config/config.py
+++ config/config.py
@@ -1,8 +1,17 @@
 # coding: utf-8
-snowflake_config = {
+snowflake_dev_config = {
     "account": "dq78402.ap-southeast-1",
     "user": "KETTLE_TEST",
     "password": "Scut2023",
     "database": "KETTLE_TEST"
 }
+snowflake_prd_config = {
+    "user": "ETL_USER",
+    "password": "WD6gvmPY",
+    "account": "ul51368.ap-southeast-1",
+    "database": "ODS",
+    "schema": "CRM",
+    "openid": "5641776398931134667",
+    "appkey": "DMVtcNFzbZgFqK03_Y"
 
+}
---
+++ request_qince_api.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+from requestAPI import RequestHandler
+import json
+from md5 import md5_string
+from datetime import datetime
+import random
+
+
+class Qince_API:
+
+    def __init__(self, path, openid, appkey, body: dict):
+        self.uri = 'https://openapi.waiqin365.com'
+        self.url = self.uri + path
+        self.headers = {"Content-Type": "application/json"}
+        self.openid = openid
+        self.appkey = appkey
+        self.body = json.dumps(body)
+
+    def generate_timestamp():
+        now = datetime.now()
+        date_time = now.strftime("%Y%m%d%H%M%S")
+        return date_time
+
+    def make_api_url(self):
+        timestamp = self.generate_timestamp()
+        json_string = self.body + '|' + self.appkey + '|' + timestamp
+        digest = md5_string(json_string)
+        msg_id = str(random.randint(10000000000, 99999999999))
+        api_url = self.url + '/' + self.openid + '/' + \
+            timestamp + '/' + digest + '/' + msg_id
+        return api_url
+
+    def request_data(self):
+
+        api_url = self.make_api_url()
+        res = RequestHandler().post(api_url, data=self.body, headers=self.headers)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            print(
+                f"Error {res.status_code}: Unable to fetch data from the API")
+            exit()
