64a44d1bbf42f55a30f7dc644e3c8f678f0ea251
chikwong <527439024@qq.com>
2025-04-10 17:06:20 +0800

    select_data

--- extract_data/extract_visit_subtask_content.py
+++ extract_data/extract_visit_subtask_content.py
@@ -6,63 +6,114 @@ import json
 import datetime
 from create_table import CreateTable
 from dynamic_merge import dynamic_merge
+PREFIX_TABLE_NAME = 'ODS.CRM.ODS_T_CRM_SUBTASK_CONTENT'
 
 
 class Extract_Subtaks_Content:
-    def __init__(self, function_list, date_start, config):
-        self.function_list = function_list
+    def __init__(self,  date_start, config):
+        # self.function_list = function_list
         self.date_start = date_start
         self.date_end = datetime.datetime.today().strftime('%Y-%m-%d')
-        self.page = 1
+        # self.page = 1
         self.rows = 1000
         self.session = create_session(config)
 
+    def get_function_list(self):
+        query = """
+                select distinct
+                    function_id,
+                    table_name
+                from
+                    ods.crm.ods_t_crm_subtask_settings;"""
+        res = self.session.sql(query).collect()
+        return res
+
+    def table_exists(self, table_name: str) -> bool:
+        """
+        检查表是否存在
+        :param session: Snowpark Session 对象
+        :param table_name: 表名（区分大小写）
+        :param schema_name: Schema 名（可选，默认为当前 Session 的 Schema）
+        :param database_name: 数据库名（可选，默认为当前 Session 的 Database）
+        :return: True 表示存在，False 表示不存在
+        """
+        database_name = table_name.split('.')[0]
+        schema_name = table_name.split('.')[1]
+        table_name = table_name.split('.')[2]
+        # 动态构造查询条件
+        conditions = []
+        if database_name:
+            conditions.append(f"table_catalog = '{database_name.upper()}'")
+        if schema_name:
+            conditions.append(f"table_schema = '{schema_name.upper()}'")
+        conditions.append(f"table_name = '{table_name.upper()}'")
+
+        query = f"""
+            SELECT COUNT(*) AS CNT
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE {" AND ".join(conditions)}
+        """
+
+        result = self.session.sql(query).collect()
+        return result[0]['CNT'] > 0
+
     def extract_data(self):
-        keep = True
-        for function_id in self.function_list.get('function_id', []):
+
+        function_lists = self.get_function_list()
+        for function_list in function_lists:
+            page = 1
+            function_id, table_name = function_list
+            keep = True
             print('正在获取function_id为{0}的数据'.format(function_id))
+            print('正在获取table_name为{0}的数据'.format(table_name))
+            full_table_name = PREFIX_TABLE_NAME + '_' + table_name.upper()
+            full_table_name_tmp = PREFIX_TABLE_NAME + '_' + table_name.upper() + '_TMP'
             while keep:
 
-                print('正在获取第{0}页数据'.format(self.page))
+                print('正在获取第{0}页数据'.format(page))
                 request_param = {
                     'function_id': function_id,
                     'date_start': self.date_start,
                     'date_end': datetime.datetime.today().strftime('%Y-%m-%d'),
-                    # 'date_end': '2025-03-02',
-                    'page': self.page,
+                    # 'date_end': self.date_start,
+                    'page': page,
                     'rows': self.rows
                 }
-
+                print(request_param)
                 res = Qince_API('/api/cusVisit/v1/queryCusVisitDetail',
                                 snowflake_prd_config, request_param).request_data()
 
                 res = res.get('response_data', None)
-                # print(res)
+
                 if res:
 
                     if res != '[]':
-                        print('第{0}页数据获取成功'.format(self.page))
+                        print('第{0}页数据获取成功'.format(page))
                         df_data = pd.json_normalize(json.loads(res))
                         df_data['function_id'] = function_id
                         df_data.columns = [col.upper()
                                            for col in df_data.columns]
-                        CreateTable('ODS.CRM.ODS_T_CRM_SUBTASK_CONTENT_' +
-                                    str(function_id)+'_TMP').create_table(df_data)
-                        dynamic_merge(session=self.session, target_table_name='ODS.CRM.ODS_T_CRM_SUBTASK_CONTENT'+'_'+function_id,
-                                      source_table_name='ODS.CRM.ODS_T_CRM_SUBTASK_CONTENT_'+str(function_id)+'_TMP', keys=['CREATE_TIME', 'VISIT_IMPLEMENT_ID', 'FUNCTION_ID', 'ID'])
+                        # 合并数据前检查table 是否存在
+                        if self.table_exists(full_table_name):
 
-                        self.page += 1
+                            CreateTable(
+                                full_table_name_tmp).create_table(df_data)
+                            dynamic_merge(session=self.session, target_table_name=full_table_name,
+                                          source_table_name=full_table_name_tmp, keys=['CREATE_TIME', 'VISIT_IMPLEMENT_ID', 'FUNCTION_ID', 'ID'])
+                        else:
+                            CreateTable(full_table_name).create_table(df_data)
+                        page += 1
 
                     else:
-                        print('第{0}页没有数据'.format(self.page))
+                        print('第{0}页没有数据'.format(page))
                         keep = False
                 else:
-                    print('第{0}页数据获取失败'.format(self.page))
+                    print('第{0}页数据获取失败'.format(page))
                     keep = False
 
 
 if __name__ == '__main__':
 
     extract_handle = Extract_Subtaks_Content(
-        function_list=function_list, date_start='2025-03-01', config=snowflake_prd_config)
+        date_start='2023-10-01', config=snowflake_prd_config)
     extract_handle.extract_data()
--- test.sql
+++ test.sql
@@ -1,52 +1,41 @@
 select
-    data.value['task_name']::string as task_name,
-    data.value['appraiser_source_code']::string as appraiser_source_code,
-    data.value['appraise_time']::string as appraise_time,
-    data.value['modifier_name']::string as modifier_name,
-    data.value['modify_time']::string as modify_time,
-    data.value['visitor_id']::string as visitor_id,
-    data.value['appraiser_id']::string as appraiser_id,
-    data.value['visitor_source_code']::string as visitor_source_code,
-    data.value['visit_type']::string as visit_type,
-    data.value['task_id']::string as task_id,
-    data.value['appraise_form_id']::string as appraise_form_id,
-    data.value['visitor_code']::string as visitor_code,
-    data.value['appraise_record_id']::string as appraise_record_id,
-    data.value['visit_use_time']::string as visit_use_time,
-    data.value['sub_task_name']::string as sub_task_name,
-    data.value['customer_code']::string as customer_code,
-    data.value['leave_time']::string as leave_time,
-    data.value['customer_source_code']::string as customer_source_code,
-    data.value['appraiser_name']::string as appraiser_name,
-    data.value['modifier_source_code']::string as modifier_source_code,
-    data.value['visitor_name']::string as visitor_name,
-    data.value['arrive_time']::string as arrive_time,
-    data.value['arrive_pos_offset']::string as arrive_pos_offset,
-    data.value['appraise_status']::string as appraise_status,
-    data.value['appraise_use_time']::string as appraise_use_time,
-    data.value['sub_task_id']::string as sub_task_id,
-    data.value['modifier_id']::string as modifier_id,
-    data.value['modifier_code']::string as modifier_code,
-    data.value['appraiser_code']::string as appraiser_code,
-    data.value['customer_name']::string as customer_name,
-    data.value['visit_id']::string as visit_id,
-    data.value['customer_id']::string as customer_id,
-    data.value['visit_date']::string as visit_date,
-    data.value['leave_pos_offset']::string as leave_pos_offset
+    task_id,
+    sub_task_id,
+    visit_id,
+    customer_code,
+    customer_id
 from
-    ods.crm.ods_t_crm_extract_original_data,
-    lateral flatten(input => parse_json(extracted_result)) as data
+    ods.crm.ods_t_crm_visit_comment_record
 where
-    method = '/api/cusVisit/v1/queryVisitApprovalByRecord'
-    and method_mode = 'VISIT'
-    and is_proccessed = false
-    -- and data.value['customer_name']::string = '煊超市（金贸时代）'
-    and data.value['task_id']::string in ('4898075112474268309')
-    and data.value['sub_task_id'] in ('9178422631309639175', '4950163845790505522');
+    sub_task_id = '9178422631309639175';
 
 select
     *
 from
-    ods.crm.ODS_T_CRM_VISIT_COMMENT_RECORD
+    ods.crm.ods_t_crm_subtask_settings
+where
+    function_id = '9178422631309639175'
 limit
     100;
+
+select
+    *
+from
+    ods.crm.ods_t_crm_subtask_content_slfdf_2503030006
+where
+    visit_implement_id = '4761343198861095962';
+
+-- ods.crm.ods_t_crm_subtask_content_slfdf_2503030006
+select
+    header.*,
+    items.*,
+    settings.columns
+from
+    ods.crm.ods_t_crm_visit_comment_record as header
+    inner join ods.crm.ods_t_crm_subtask_settings as settings on header.sub_task_id = settings.function_id
+    inner join IDENTIFIER (
+        'ods.crm.ods_t_crm_subtask_content_slfdf_2503030006'
+    ) as items on header.visit_id = items.visit_implement_id
+    and settings.function_id = items.function_id
+where
+    header.visit_id = '4761343198861095962';
